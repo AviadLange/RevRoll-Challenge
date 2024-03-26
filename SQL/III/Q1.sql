f6cd93b586809eba17dbb4d894716afb1c8c22cd
Aviad <134489612+AviadLange@users.noreply.github.com>
2024-03-26 16:09:16 +0200

    Update Q1.sql

--- SQL/III/Q1.sql
+++ SQL/III/Q1.sql
@@ -1,1 +1,10 @@
-
+-- This query filters for only participated installers.
+SELECT DISTINCT i1.name
+FROM install_derby AS i_d
+INNER JOIN installers AS i1 -- Keeps only participated installers.
+ON i1.installer_id = i_d.installer_one_id
+UNION
+SELECT DISTINCT i2.name
+FROM install_derby AS i_d
+INNER JOIN installers AS i2 -- Keeps only participated installers.
+ON i2.installer_id = i_d.installer_two_id;
